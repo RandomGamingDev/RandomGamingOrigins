a13582f04db2dfb3ea329b08bc08e012ba7de186
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-03-19 09:58:23 -0400

    underflow error in deserialization fixed

--- src/main/java/me/randomgamingdev/randomgamingorigins/Origins.java
+++ src/main/java/me/randomgamingdev/randomgamingorigins/Origins.java
@@ -500,7 +500,7 @@ public class Origins implements Listener {
 
                         while (myReader.hasNextLine()) {
                             line = myReader.nextLine();
-                            if (line.charAt(0) == 'P')
+                            if (line.charAt(6) == ':')
                                 break;
                             invStr.append(line + '\n');
                         }
